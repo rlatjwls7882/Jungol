7333d9cf3d384919bbd5048b5e6e0622cd13a8fe
Seojin Kim <rlatjwls7882@gmail.com>
2026-05-24 17:00:24 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -33,21 +33,23 @@ EXT = {
 
 def get_problem_tier(level):
     tier = {
-        0: "Unrated",
         1: "Bronze 5", 2: "Bronze 4", 3: "Bronze 3", 4: "Bronze 2", 5: "Bronze 1",
         6: "Silver 5", 7: "Silver 4", 8: "Silver 3", 9: "Silver 2", 10: "Silver 1",
         11: "Gold 5", 12: "Gold 4", 13: "Gold 3", 14: "Gold 2", 15: "Gold 1",
         16: "Platinum 5", 17: "Platinum 4", 18: "Platinum 3", 19: "Platinum 2", 20: "Platinum 1",
         21: "Diamond 5", 22: "Diamond 4", 23: "Diamond 3", 24: "Diamond 2", 25: "Diamond 1",
-        26: "Ruby 5", 27: "Ruby 4", 28: "Ruby 3", 29: "Ruby 2", 30: "Ruby 1"
+        26: "Ruby 5", 27: "Ruby 4", 28: "Ruby 3", 29: "Ruby 2", 30: "Ruby 1",
     }
 
     try:
         level = int(level)
     except Exception:
-        return "Unrated"
+        return ""
 
-    return tier.get(level, "")
+    if level not in tier:
+        return ""
+
+    return f'<img alt="{tier[level]}" src="assets/{level}.svg" height="24">'
 
 def request_html(url):
     response = requests.get(
