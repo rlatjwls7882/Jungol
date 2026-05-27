899feea66b22339069d79bbf13e9f887307e951c
Seojin Kim <rlatjwls7882@gmail.com>
2026-05-27 23:56:18 +0900

    Create 12422.cpp

---
+++ 12xxx/12422.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rep(x) for(int __i=(x);__i>0;--__i)
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef __int128 i128;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    while(true) {
+        int a, b; cin >> a >> b;
+        if(cin.fail()) break;
+        if(a<2 || a>9 || b<2 || b>9) {
+            cout << "INPUT ERROR!\n";
+            continue;
+        }
+
+        if(a<b) {
+            for(int i=a;i<=b;i++) {
+                for(int j=1;j<=9;j++) cout << i << " * " << j << " = " << i*j << '\n';
+                cout << '\n';
+            }
+        } else {
+            for(int i=a;i>=b;i--) {
+                for(int j=1;j<=9;j++) cout << i << " * " << j << " = " << i*j << '\n';
+                cout << '\n';
+            }
+        }
+    }
+}
