c0196a21320fe6d201ad5fbf82ebe31dc1e0e2f5
Seojin Kim <rlatjwls7882@gmail.com>
2026-07-05 07:14:13 +0900

    Create 8057.cpp

---
+++ 08xxx/8057.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rep(x) for(int __i=(x);__i>0;--__i)
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef __int128 i128;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+struct point {
+    ll x, y;
+};
+
+ll ccw(point a, point b, point c) {
+    point v1 = {a.x-b.x, a.y-b.y};
+    point v2 = {a.x-c.x, a.y-c.y};
+    return v1.x*v2.y-v2.x*v1.y;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m, q; cin >> n >> m >> q;
+    vector<pair<point, point>> v(m);
+    for(int i=0;i<m;i++) cin >> v[i].x.x >> v[i].x.y >> v[i].y.x >> v[i].y.y;
+    while(q--) {
+        point a, b; cin >> a.x >> a.y >> b.x >> b.y;
+        bool chk=true;
+        for(auto [aa, bb]:v) {
+            ll r1=ccw(aa, bb, a);
+            ll r2=ccw(aa, bb, b);
+            if(r1<=0 && r2>=0 || r1>=0 && r2<=0) {
+                chk=false;
+                break;
+            }
+        }
+        cout << (chk ? "1\n" : "0\n");
+    }
+}
